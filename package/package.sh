24df2f7dd5f86ac72a8cb50d0f5e1d366599fb82
Tahina Ramananandro <taramana@microsoft.com>
2020-05-06 15:15:03 -0700

    move Dockerfile out of its absurd location

--- package/package.sh
+++ package/package.sh
@@ -3,12 +3,8 @@
 set -e
 
 NAME=quic-crypto
-ARCHIVE=$NAME.tar
+ARCHIVE=$NAME.tar.bz2
 
 ln -s .. $NAME
-tar cf $ARCHIVE $NAME/src/QUIC*.fst $NAME/src/QUIC*.fsti $NAME/src/Makefile $NAME/test/* $NAME/Makefile* $NAME/README.md $NAME/package/fstar.sh $NAME/package/*.el
+tar cjf $ARCHIVE $NAME/Dockerfile $NAME/src/QUIC*.fst $NAME/src/QUIC*.fsti $NAME/src/Makefile $NAME/test/* $NAME/Makefile* $NAME/README.md $NAME/package/fstar.sh $NAME/package/*.el
 rm $NAME
-ln -s . $NAME
-tar rf $ARCHIVE $NAME/Dockerfile
-rm $NAME
-bzip2 $ARCHIVE
