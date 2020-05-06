56f2bf96f189f279deb06b65e5f074982f52d5cb
Tahina Ramananandro <taramana@microsoft.com>
2020-05-06 08:29:15 -0700

    forgot src/Makefile for the F* mode

--- package/package.sh
+++ package/package.sh
@@ -6,7 +6,7 @@ NAME=quic-crypto
 ARCHIVE=$NAME.tar
 
 ln -s .. $NAME
-tar cf $ARCHIVE $NAME/src/QUIC*.fst $NAME/src/QUIC*.fsti $NAME/test/* $NAME/Makefile* $NAME/README.md $NAME/package/fstar.sh $NAME/package/*.el
+tar cf $ARCHIVE $NAME/src/QUIC*.fst $NAME/src/QUIC*.fsti $NAME/src/Makefile $NAME/test/* $NAME/Makefile* $NAME/README.md $NAME/package/fstar.sh $NAME/package/*.el
 rm $NAME
 ln -s . $NAME
 tar rf $ARCHIVE $NAME/Dockerfile
