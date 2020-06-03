cf550a07034b537da17471d2948f27c440f2db65
Tahina Ramananandro <taramana@microsoft.com>
2020-06-03 11:24:50 -0700

    anon

--- package/package.sh
+++ package/package.sh
@@ -6,5 +6,5 @@ NAME=quic-crypto
 ARCHIVE=$NAME.tar.bz2
 
 ln -s .. $NAME
-tar cjf $ARCHIVE $NAME/Dockerfile $NAME/src/QUIC*.fst $NAME/src/QUIC*.fsti $NAME/src/Makefile $NAME/test/* $NAME/Makefile* $NAME/README.md $NAME/package/fstar.sh $NAME/package/*.el $(if [[ -d $NAME/dist ]] ; then echo $NAME/dist/ ; fi)
+tar cjf $ARCHIVE $NAME/Dockerfile $NAME/src/QUIC*.fst $NAME/src/QUIC*.fsti $NAME/src/Makefile $NAME/test/* $NAME/Makefile* $NAME/README.md $NAME/package/fstar.sh $NAME/package/*.el $(for f in $NAME/dist/*.c $NAME/dist/*.h $NAME/dist/*.def $NAME/dist/Makefile.* ; do if [[ -e $f ]] ; then echo $f ; fi ; done)
 rm $NAME
